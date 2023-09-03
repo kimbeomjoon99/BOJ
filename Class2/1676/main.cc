3587843721eff4679415c1c078a052925af571cb
kimbeomjoon99 <102931473+kimbeomjoon99@users.noreply.github.com>
2023-09-03 17:01:40 +0900

    Create main.cc

---
+++ Class2/1676/main.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <deque>
+typedef long long ll;
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    
+    ll n, i;
+    cin >> n;
+    cout << (n / 5) + (n / 25) + (n / 125) << "\n";
+
+    return 0;
+}
