ab65ee73ce0611766ccb50f3c4582b97d824ae85
kimbeomjoon99 <102931473+kimbeomjoon99@users.noreply.github.com>
2023-09-03 18:13:00 +0900

    Create main.cc

---
+++ Class4/15657/main.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pii;
+#define INF 999999999;
+
+ll n, m, visited[9];
+vector<ll> arr, nums;
+
+void bt(ll depth, ll num){
+    if (depth == m){
+        for (ll i = 0; i < arr.size(); i++)
+            cout <<  arr[i] << " ";
+        cout << "\n";
+        return;
+    }
+    for (ll i = num; i < n; i++){
+        //if (visited[i]) continue;
+        //visited[i] = 1;
+        arr.push_back(nums[i]);
+        bt(depth + 1, i);
+        arr.pop_back();
+        //visited[i] = 0;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    
+    cin >> n >> m;
+    for (ll i = 0; i < n; i++){
+        ll tmp;
+        cin >> tmp;
+        nums.push_back(tmp);
+    }
+    sort(nums.begin(), nums.end());
+    bt(0, 0);
+
+    return 0;
+}
