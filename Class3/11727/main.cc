d96cefc23542a52be020da3243ae33f0239ff4a9
kimbeomjoon99 <102931473+kimbeomjoon99@users.noreply.github.com>
2023-09-03 17:41:51 +0900

    Create main.cc

---
+++ Class3/11727/main.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <set>
+#include <map>
+typedef long long ll;
+using namespace std;
+ll dp[1001];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    
+    ll n, i;
+    dp[1] = 1;
+    dp[2] = 3;
+    cin >> n;
+    for (i = 3; i <= n; i++){
+        dp[i] = ((2 * (dp[i - 2] % 10007)) + (dp[i - 1] % 10007)) % 10007;
+    }
+    cout << dp[n] << "\n";
+
+    return 0;
+}
