ce4d29d1b41caa4bd5c97d7a3e49034458416fcc
kimbeomjoon99 <102931473+kimbeomjoon99@users.noreply.github.com>
2023-09-03 17:07:25 +0900

    Create main.cc

---
+++ Class2/11651/main.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <deque>
+typedef long long ll;
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    
+    ll n, i, j;
+    cin >> n;
+    vector<pair<ll, ll>> locs;
+    for (i = 0; i < n; i++){
+        ll a, b;
+        cin >> a >> b;
+        locs.push_back({b, a});
+    }
+    sort(locs.begin(), locs.end());
+    for (i = 0; i < n; i++){
+        cout << locs[i].second << " " << locs[i].first << "\n";
+    }
+
+    return 0;
+}
